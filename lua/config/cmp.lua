38e40312ffd3a1581844d5d7519bd2c8cf58e62a
Niklas Larsson <nlarzon@gmail.com>
2021-09-22 21:40:06 +0200

    Update cmp config
    Remove faulty lspconfig

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -37,8 +37,4 @@
       { name = 'path' },
     }
   })
-
-  -- Setup lspconfig.
-  require('lspconfig')[%YOUR_LSP_SERVER%].setup {
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   }
